892d2543d6082ac437543e38d8faf8c6777c4106
r-yvan <yvankiliye.rubuto@gmail.com>
2024-12-14 17:13:46 +0200

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import v1 from "../assets/v-1.png";
+import v2 from "../assets/v-2.png";
+import v3 from "../assets/v-3.png";
+import v4 from "../assets/v-4.png";
+import v5 from "../assets/v-5.png";
 import background from "../assets/BackgroundImage.png";
 import profile from "../assets/anotherMe.png";
 import netflixLogo from "../assets/netflix.png";
@@ -54,47 +59,67 @@ const Header = () => {
       </div>
       <div className="spotlight" ref={ref} animate={controls}>
         <div className="flex flex-col relative px-4 py-40 pb-96 z-10">
-          <div className="w-full flex justify-between px-28">
-            <div className="w-1/3 gap-2">
-              <div className="px-3 py-2 bg-[#202020] w-fit rounded-xl">
-                <p className="font-bold">Hello There, I' m</p>
+          <div className="w-full flex justify-between px-36 pr-56">
+            <div className="w-1/3 flex flex-col gap-6 mt-10">
+              <div>
+                <div className="px-3 py-2 bg-[#202020] w-fit rounded-xl">
+                  <p className="font-bold">Hello There, I' m</p>
+                </div>
+                <div>
+                  <p className="font-bold text-3xl">Rubuto</p>
+                  <p className="font-bold text-green-400 text-2xl">Yvan</p>
+                </div>
               </div>
               <div>
-                <p className="font-bold text-3xl">Rubuto</p>
-                <p className="font-bold text-green-400 text-2xl">Yvan</p>
+                <p className="w-2/3">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolorem totam adipisci dolor? Laboriosam eligendi vel dolorum,
+                  fuga non pariatur quae?
+                </p>
+                <button className="font-bold text-green-400 border border-green-400 px-10 py-3 rounded-2xl">
+                  Contact Me
+                </button>
               </div>
-              <p className="w-2/3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-                totam adipisci dolor? Laboriosam eligendi vel dolorum, fuga non
-                pariatur quae?
-              </p>
-              <button className="font-bold text-green-400 border border-green-400 px-10 py-3 rounded-2xl">
-                Contact Me
-              </button>
-              <div className="px-3 py-2 bg-[#202020] w-fit rounded-xl">
-                <p className="font-bold">My Social Media</p>
-              </div>
-              <div></div>
-              <div className="flex flex-row">
-                <div className="px-3 border-r border-green-400">
-                  <p>LinkedIn</p>
+              <div>
+                <div className="px-3 py-2 bg-[#202020] w-fit rounded-xl">
+                  <p className="font-bold">My Social Media</p>
                 </div>
-                <div className="px-3 border-r border-green-400">
-                  <p>Facebook</p>
-                </div>
-                <div className="px-3">
-                  <p>Instagram</p>
+                <div className="flex flex-row">
+                  <div className="px-3 border-r border-green-400">
+                    <p>LinkedIn</p>
+                  </div>
+                  <div className="px-3 border-r border-green-400">
+                    <p>Facebook</p>
+                  </div>
+                  <div className="px-3">
+                    <p>Instagram</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="w-[500px] h-[500px] absolute right-[42%] top-[40] rounded-full bg-green-400"></div>
+            <div className="w-[500px] h-[500px] absolute right-[43%] top-[40] rounded-full bg-green-400"></div>
             <img
               src={profile}
               alt=""
-              className="w-[500px] absolute h-auto top-54 right-[42%]"
+              className="w-[500px] absolute h-auto top-54 right-[43%]"
             />
-            <div className="w-1/3">
-              <div className="flex flex-col gap-6">
+            <img
+              src={v5}
+              alt=""
+              className="absolute right-[300px] w-80 h-7 top-[245px]"
+            />
+            <img
+              src={v2}
+              alt=""
+              className="absolute left-10 top-[200px] w-16 h-auto"
+            />
+            <img
+              src={v4}
+              alt=""
+              className="absolute left-[90px] top-[420px] w-[300px] opacity-50 h-auto"
+            />
+            <div className="flex flex-col gap-40 w-1/3 mt-20">
+              <div className="flex flex-col gap-10">
                 <div>
                   <p className="font-bold text-green-400 text-3xl">
                     UI/UX Designer,{" "}
@@ -125,6 +150,13 @@ const Header = () => {
                     </div>
                   </div>
                 </div>
+              </div>
+              <div className="w-4/5 pl-5 border-l border-green-400 ml-32">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Fugiat similique quod, cum repudiandae quisquam laborum eaque
+                  modi architecto at vel beatae. Non deserunt laudantium
+                </p>
               </div>
             </div>
           </div>
