b8dc82270c15f5456575561c39927f0126c5dafa
r-yvan <yvankiliye.rubuto@gmail.com>
2024-12-15 20:20:08 +0200

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,10 +57,10 @@ const Header = () => {
           </ul>
         </nav>
       </div>
-      <div className="spotlight" ref={ref} animate={controls}>
+      <div ref={ref} animate={controls}>
         <div className="flex flex-col relative px-4 py-40 pb-96 z-10">
-          <div className="w-full flex justify-between px-36 pr-56">
-            <div className="w-1/3 flex flex-col gap-6 mt-10">
+          <div className="w-full flex justify-between px-28 pl-32">
+            <div className="w-1/3 flex flex-col gap-6 mt-16">
               <div>
                 <div className="px-3 py-2 bg-[#202020] w-fit rounded-xl">
                   <p className="font-bold">Hello There, I' m</p>
@@ -70,13 +70,13 @@ const Header = () => {
                   <p className="font-bold text-green-400 text-2xl">Yvan</p>
                 </div>
               </div>
-              <div>
-                <p className="w-2/3">
+              <div className="flex flex-col gap-3">
+                <p className="w-4/5">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Dolorem totam adipisci dolor? Laboriosam eligendi vel dolorum,
                   fuga non pariatur quae?
                 </p>
-                <button className="font-bold text-green-400 border border-green-400 px-10 py-3 rounded-2xl">
+                <button className="font-bold text-green-400 border border-green-400 w-fit px-10 py-3 rounded-2xl">
                   Contact Me
                 </button>
               </div>
@@ -97,28 +97,30 @@ const Header = () => {
                 </div>
               </div>
             </div>
-            <div className="w-[500px] h-[500px] absolute right-[43%] top-[40] rounded-full bg-green-400"></div>
+            <div className="w-[550px] h-[550px] absolute right-[40%] top-[40] rounded-full p-5 bg-green-400">
+              <div className="w-full h-full bg-green-400 border-[15px] border-[#0A0A0A] rounded-full"></div>
+            </div>
             <img
               src={profile}
               alt=""
-              className="w-[500px] absolute h-auto top-54 right-[43%]"
+              className="w-[500px] absolute h-auto top-56 right-[40%] bg-gradient-to-b from-inherit to-slate-700"
             />
             <img
               src={v5}
               alt=""
-              className="absolute right-[300px] w-80 h-7 top-[245px]"
+              className="absolute right-[350px] w-80 h-7 top-[245px]"
             />
             <img
               src={v2}
               alt=""
-              className="absolute left-10 top-[200px] w-16 h-auto"
+              className="absolute left-[60px] top-[250px] w-14 rotate-[5deg] h-[70px]"
             />
             <img
-              src={v4}
+              src={v3}
               alt=""
-              className="absolute left-[90px] top-[420px] w-[300px] opacity-50 h-auto"
+              className="absolute left-[170px] rotate-[3deg] top-[590px] w-48 h-auto"
             />
-            <div className="flex flex-col gap-40 w-1/3 mt-20">
+            <div className="flex flex-col gap-40 w-1/3">
               <div className="flex flex-col gap-10">
                 <div>
                   <p className="font-bold text-green-400 text-3xl">
@@ -159,23 +161,6 @@ const Header = () => {
                 </p>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center gap-5 falling-text">
-          <div className="flex justify-center items-center flex-col w-52 h-52 border border-gray-400 px-5 py-5 bg-white bg-opacity-15 backdrop-filter backdrop-blur-3xl rounded-2xl duration-200 hover:scale-95 hover:border-green-400">
-            <h1 className="text-6xl items-center text-green-400">1+</h1>
-            <p className="opacity-70 font-light pt-3">Years</p>
-            <p>Experience</p>
-          </div>
-          <div className="flex justify-center flex-col items-center w-52 h-52 border px-5 py-5 bg-white bg-opacity-15 backdrop-filter backdrop-blur-3xl rounded-2xl border-gray-400 duration-200 hover:scale-95 hover:border-green-400">
-            <h1 className="text-6xl items-center text-green-400">21+</h1>
-            <p className="opacity-70 font-light pt-3">Github</p>
-            <p className="font-bold">Repositories</p>
-          </div>
-          <div className="flex justify-center flex-col items-center w-52 h-52 border px-5 py-5 bg-white bg-opacity-15 backdrop-filter backdrop-blur-3xl rounded-2xl border-gray-400 duration-200 hover:scale-95 hover:border-green-400">
-            <h1 className="text-6xl items-center text-green-400">71+</h1>
-            <p className="opacity-70 font-light pt-3">Friendly</p>
-            <p className="font-bold">Connections</p>
           </div>
         </div>
       </div>
